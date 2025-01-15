74c22b9952853aa96fe27942044cec15c22ae372
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-01-14 20:07:14 -0500

    loading page fades out after 1200 seconds

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-$(window).on('load', function() {
-    // Optionally, you can introduce a slight delay before fading out to ensure everything is ready
-    setTimeout(function() {
-        $('.loading-screen').fadeOut(1000); // Fade out the loading screen and arrow
-        $('.container').fadeIn(1000); // Fade in the page content
-    }, 500); // 500ms delay to allow all resources to load
+$(window).on('load', function () {
+    // Ensure the container is hidden initially
+
+    // Fade out the loading screen
+    $('.loading-screen').fadeOut(1200);
 });
