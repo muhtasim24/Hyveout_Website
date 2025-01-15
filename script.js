0425c6f035b99fa020afab4044cc1db8dbe86799
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-01-14 19:57:43 -0500

    setting timeout to wait till other parts of pages load in first

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-$(window).on('load', function() {
-    $('.container').hide();
-    $('.loading-screen').fadeOut(1000);
-    $('.container').fadeIn(1000);
-})
+$(window).on('load', function () {
+    // Wait a bit before fading out the loading screen
+    setTimeout(function() {
+        $('.loading-screen').fadeOut(1000, function () {
+            $('.container').fadeIn(1000);
+        });
+    }, 300); // 500ms delay before starting the fade-out
+});
