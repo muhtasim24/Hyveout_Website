cdee91699e86e81cb3bdf1b19adcfade8a134025
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-01-14 20:12:08 -0500

    fading in the container

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(window).on('load', function () {
     // Ensure the container is hidden initially
+    $('.container').hide()
 
     // Fade out the loading screen
-    $('.loading-screen').fadeOut(1200);
+    $('.loading-screen').fadeOut(1200, function () {
+        // Once the loading screen fades out, display the container
+        $('.container').fadeIn(500);
+    });
 });
