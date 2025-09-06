15d2f51ac877290895130474fb0429b2bdfed3c0
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-09-06 14:59:27 -0400

    back button component and css

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,2 +1,10 @@
+import backBtn from "../gifs/backBtn.gif";
+import "../css/backBtn.css"
 
+function BackButton() {
+    return (
+        <img className="back-button" src={backBtn} alt="back button" />
+    )
+}
 
+export default BackButton;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,14 @@
+import BackButton from "../components/backButton";
+import StaticBackground from "../components/StaticBackground";
 
 
 function Contact() {
-
+    return (
+        <div className="main-border">
+            <StaticBackground/>
+            <BackButton/>
+        </div>
+    )
 }
 
 export default Contact;
