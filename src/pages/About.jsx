a7063b21f2d562a8815763bae295664588bd8bab
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-09-06 15:18:35 -0400

    about page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,18 @@
-
+import BackButton from "../components/backButton";
+import StaticBackground from "../components/StaticBackground";
+import "../css/about.css"
 
 function About() {
-
+    return (
+        <div className="main-border">
+            <StaticBackground/>
+            <BackButton/>
+            <div className="about-text">
+                <h1>HYVEOUT IS A CREATIVE AGENCY/COLLECTIVE INTERESTED IN THE INTEGRATION OF ALL FORMS OF MEDIA</h1>
+                <h1>WHAT WE ARE IS IN OUR WORK</h1>
+            </div>
+        </div>
+    )
 }
 
 export default About;
