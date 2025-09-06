302861155c6248243e1aa592b31c3168a8329297
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-09-06 15:44:35 -0400

    mind and shop done for both mobile and desktop

--- src/pages/Mind.jsx
+++ src/pages/Mind.jsx
@@ -1,7 +1,20 @@
+import BackButton from "../components/backButton";
+import StaticBackground from "../components/StaticBackground";
+import brainGif from "../gifs/brain.gif";
+import "../css/mind.css"
 
 
 function Mind() {
-
+    return (
+        <div className="main-border">
+            <StaticBackground/>
+            <BackButton/>
+            <div className="mind-container">
+                <img className="mindImg"src={brainGif} alt="brain gif"/>
+                <h1 className="mindText">COMING SOON</h1>
+            </div>
+        </div>
+    )
 }
 
 export default Mind;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,20 @@
+import BackButton from "../components/backButton";
+import StaticBackground from "../components/StaticBackground";
+import shopGif from "../gifs/shop.gif";
+import "../css/shop.css"
 
 
 function Shop() {
-
+    return (
+        <div className="main-border">
+            <StaticBackground/>
+            <BackButton/>
+            <div className="shop-container">
+                <img className="shopImg" src={shopGif} alt="brain gif"/>
+                <h1 className="shopText">COMING SOON</h1>
+            </div>
+        </div>
+    )
 }
 
 export default Shop;
