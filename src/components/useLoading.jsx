91e4e2431ed453fc896feb8ac04ff30d8070ca64
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-09-19 14:59:11 -0400

    left out a treu statement

--- src/components/useLoading.jsx
+++ src/components/useLoading.jsx
@@ -23,7 +23,7 @@ function useLoading(images = [], delay = 1000) {
             img.onload = () => {
                 loadedCount++;
                 if (loadedCount === images.length) {
-                    setImagesLoaded(false);
+                    setImagesLoaded(true);
                 }
             };
         }); 
